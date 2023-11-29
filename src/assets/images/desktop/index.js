3239446991cb34eda2ab2c44263fb13ccd0668e9
IamArshadAli <arshadalikaldane@gmail.com>
2023-11-29 22:34:00 +0530

    assets-desktop

---
+++ src/assets/images/desktop/index.js
@@ -0,0 +1,21 @@
+import desktopGalleryConeImage from "./image-gallery-cone.jpg";
+import desktopGalleryMilkbottlesImage from "./image-gallery-milkbottles.jpg";
+import desktopGalleryOrangeImage from "./image-gallery-orange.jpg";
+import desktopGallerySugarcubesImage from "./image-gallery-sugarcubes.jpg";
+import desktopGraphicDesignImage from "./image-graphic-design.jpg";
+import desktopHeaderImage from "./image-header.jpg";
+import desktopPhotographyImage from "./image-photography.jpg";
+import desktopStandOutImage from "./image-stand-out.jpg";
+import desktopTransformImage from "./image-transform.jpg";
+
+export {
+  desktopGalleryConeImage,
+  desktopGalleryMilkbottlesImage,
+  desktopGalleryOrangeImage,
+  desktopGallerySugarcubesImage,
+  desktopGraphicDesignImage,
+  desktopHeaderImage,
+  desktopPhotographyImage,
+  desktopStandOutImage,
+  desktopTransformImage,
+};
