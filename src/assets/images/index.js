beb38f3bbc91acb3b2c003761a8c87cf75384f69
IamArshadAli <arshadalikaldane@gmail.com>
2023-11-29 22:33:31 +0530

    assets-main

---
+++ src/assets/images/index.js
@@ -0,0 +1,23 @@
+import facebookIcon from "./icon-facebook.svg";
+import instagramIcon from "./icon-instagram.svg";
+import pinterestIcon from "./icon-pinterest.svg";
+import twitterIcon from "./icon-twitter.svg";
+import arrowDownIcon from "./icon-arrow-down.svg";
+import hamburgerIcon from "./icon-hamburger.svg";
+import emilyImage from "./image-emily.jpg";
+import jennieImage from "./image-jennie.jpg";
+import thomasImage from "./image-thomas.jpg";
+import logo from "./logo.svg";
+
+export {
+  facebookIcon,
+  instagramIcon,
+  pinterestIcon,
+  twitterIcon,
+  arrowDownIcon,
+  hamburgerIcon,
+  emilyImage,
+  jennieImage,
+  thomasImage,
+  logo
+};
