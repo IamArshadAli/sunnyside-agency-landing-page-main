6ed8822fae90db09c01693c07b9f64005184f505
IamArshadAli <arshadalikaldane@gmail.com>
2023-11-29 22:34:17 +0530

    assets-mobile

---
+++ src/assets/images/mobile/index.js
@@ -0,0 +1,21 @@
+import mobileGalleryConeImage from "./image-gallery-cone.jpg";
+import mobileGalleryMilkbottlesImage from "./image-gallery-milkbottles.jpg";
+import mobileGalleryOrangeImage from "./image-gallery-orange.jpg";
+import mobileGallerySugarCubesImage from "./image-gallery-sugar-cubes.jpg";
+import mobileGraphicDesignImage from "./image-graphic-design.jpg";
+import mobileHeaderImage from "./image-header.jpg";
+import mobilePhotographyImage from "./image-photography.jpg";
+import mobileStandOutImage from "./image-stand-out.jpg";
+import mobileTransformImage from "./image-transform.jpg";
+
+export {
+  mobileGalleryConeImage,
+  mobileGalleryMilkbottlesImage,
+  mobileGalleryOrangeImage,
+  mobileGallerySugarCubesImage,
+  mobileGraphicDesignImage,
+  mobileHeaderImage,
+  mobilePhotographyImage,
+  mobileStandOutImage,
+  mobileTransformImage,
+};
